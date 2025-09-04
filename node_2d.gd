5342943624a8f6c201200220c3659fe00b1605bc
NathanSimon9 <nathansimon1234567@gmail.com>
2025-09-03 21:23:49 -0400

    page couverture

--- node_2d.gd
+++ node_2d.gd
@@ -1,6 +1,12 @@
-extends Node2D
+extends Node
+# Déclare la fonction qui sera appelée lorsque le bouton est cliqué
 
 
+  
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://node_2d.tscn")
 func _on_area_2d_body_entered(body: RigidBody2D) -> void:
 	get_tree().reload_current_scene()
-	
