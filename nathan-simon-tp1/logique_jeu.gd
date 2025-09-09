96e7d4d0f84a823f1c30e5c61a2d45ac935fa869
NathanSimon9 <nathansimon1234567@gmail.com>
2025-09-09 17:24:28 -0400

    reparation

--- nathan-simon-tp1/logique_jeu.gd
+++ nathan-simon-tp1/logique_jeu.gd
@@ -22,12 +22,6 @@ func _on_area_2d_body_entered(body:Node2D) -> void:
 		get_tree().change_scene_to_file("res://robot_gagner.tscn")
 	#get_tree().reload_current_scene()
 
-func _on_body_entered(body: Node) -> void:
-	pass # Replace with function body.
-	
-
-
-
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
